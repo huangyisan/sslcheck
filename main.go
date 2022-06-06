5bb2e90734249de9221187c7a6fce11ab70dcd64
huangyisan <anonymousyisan@gmail.com>
2022-06-06 20:07:24 +0800

    add ssl check

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+)
+
+const expireDay = 10
+
+func httpsHandshake(domain string) (conn *tls.Conn, err error) {
+	conn, err = tls.Dial("tcp", domain+":443", nil)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func hostnameVerify(conn *tls.Conn, domain, hostname string) error {
+	err := conn.VerifyHostname(domain)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func expireDateVerify(conn *tls.Conn) bool {
+	// chain list, 0 -> domain ssl
+	expire := conn.ConnectionState().PeerCertificates[0].NotAfter
+	subDay := expire.Sub(time.Now()) / 24
+	if subDay <= expireDay {
+		return true
+	}
+	return false
+}
+
+func main() {
+	conn, err := tls.Dial("tcp", "www.baidu.com:443", nil)
+	if err != nil {
+		panic("Server doesn't support SSL certificate err: " + err.Error())
+	}
+	err = conn.VerifyHostname("www.baidu.com")
+	if err != nil {
+		panic("hostname 不匹配" + err.Error())
+	}
+	// 获取到证书链
+	expire := conn.ConnectionState().PeerCertificates[0].NotAfter
+	fmt.Println(expire)
+	fmt.Println(expire.Unix())
+	now := time.Now()
+	days := expire.Sub(now).Hours() / 24
+	fmt.Println(int(days))
+}
