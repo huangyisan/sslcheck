3128dcd88ab45961a8595ec2307eefac7686af95
huangyisan <anonymousyisan@gmail.com>
2022-06-06 21:56:31 +0800

    fix hour

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"crypto/tls"
 	"fmt"
 	. "sslcheck/config"
+	"sync"
 	"time"
 )
 
 const expireDay = 10
+
+var (
+	wg sync.WaitGroup
+)
 
 func httpsHandshake(domain string) (conn *tls.Conn, err error) {
 	conn, err = tls.Dial("tcp", domain+":443", nil)
@@ -17,7 +22,7 @@ func httpsHandshake(domain string) (conn *tls.Conn, err error) {
 	return conn, nil
 }
 
-func hostnameVerify(conn *tls.Conn, domain, hostname string) error {
+func hostnameVerify(conn *tls.Conn, domain string) error {
 	err := conn.VerifyHostname(domain)
 	if err != nil {
 		return err
@@ -28,7 +33,7 @@ func hostnameVerify(conn *tls.Conn, domain, hostname string) error {
 func expireDateVerify(conn *tls.Conn) bool {
 	// chain list, 0 -> domain ssl
 	expire := conn.ConnectionState().PeerCertificates[0].NotAfter
-	subDay := expire.Sub(time.Now()) / 24
+	subDay := expire.Sub(time.Now()).Hours() / 24
 	if subDay <= expireDay {
 		return true
 	}
@@ -37,20 +42,46 @@ func expireDateVerify(conn *tls.Conn) bool {
 
 func main() {
 	InitAll()
-	fmt.Println(Conf.GetString("expireDay"))
-	conn, err := tls.Dial("tcp", "www.baidu.com:443", nil)
-	if err != nil {
-		panic("Server doesn't support SSL certificate err: " + err.Error())
+
+	domainList := Conf.GetStringSlice("domains")
+	fmt.Println(domainList)
+	successChan := make(chan string, len(domainList))
+	failedChan := make(chan string, len(domainList))
+	expireChan := make(chan string, len(domainList))
+	for _, domain := range domainList {
+		wg.Add(1)
+		testDomain := domain
+		go func(testDomain string) {
+			defer wg.Done()
+			conn, err := httpsHandshake(testDomain)
+			if err != nil {
+				failedChan <- testDomain
+				return
+			}
+			err = hostnameVerify(conn, testDomain)
+			if err != nil {
+				failedChan <- testDomain
+				return
+			}
+			if expireDateVerify(conn) {
+				expireChan <- testDomain
+				return
+			} else {
+				successChan <- testDomain
+				return
+			}
+		}(testDomain)
 	}
-	err = conn.VerifyHostname("www.baidu.com")
-	if err != nil {
-		panic("hostname 不匹配" + err.Error())
+
+	go func() {
+		wg.Wait()
+		close(successChan)
+		close(failedChan)
+		close(expireChan)
+	}()
+
+	for v := range expireChan {
+		fmt.Printf("%v\n", v)
 	}
-	// 获取到证书链
-	expire := conn.ConnectionState().PeerCertificates[0].NotAfter
-	fmt.Println(expire)
-	fmt.Println(expire.Unix())
-	now := time.Now()
-	days := expire.Sub(now).Hours() / 24
-	fmt.Println(int(days))
+
 }
